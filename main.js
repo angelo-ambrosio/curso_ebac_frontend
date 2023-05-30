2f060507b3e941eb00817d1f4280c90ef99ec5f7
Angelo Ambrósio <angelotosta1105@gmail.com>
2023-05-30 12:20:32 -0300

    exercício finalizado

---
+++ main.js
@@ -0,0 +1,5 @@
+alert('O grunt...'),
+alert('... está funcionando!')
+alert('Mensagem secreta no console.log!!!')
+
+console.log("Terminei o módulo e estou um passo mais próximo do meu emprego. Obrigado")
